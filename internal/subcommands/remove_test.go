1f988cb3fef56848220a6f2eaa545a01bd399386
Andrew Albertson <adavida@mac.com>
2020-10-06 21:54:32 -0600

    Add test for remove arg parsing

---
+++ internal/subcommands/remove_test.go
@@ -0,0 +1,30 @@
+package subcommands
+
+import (
+	"strings"
+	"reflect"
+	"testing"
+)
+
+func TestParseRemoveArgs(t *testing.T) {
+	tests := []struct {
+		name        string
+		args        string
+		wantAliases []string
+		wantErr     bool
+	}{
+		{"happy path", "da sn", []string{"da", "sn"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotAliases, err := ParseRemoveArgs(strings.Split(tt.args, " "))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseRemoveArgs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotAliases, tt.wantAliases) {
+				t.Errorf("ParseRemoveArgs() = %v, want %v", gotAliases, tt.wantAliases)
+			}
+		})
+	}
+}
