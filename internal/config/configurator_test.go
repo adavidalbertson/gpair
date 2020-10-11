6054853a9526e267f7adb81e96f2f5b299ebe131
Andrew Albertson <adavida@mac.com>
2020-10-11 15:34:11 -0600

    Add configurator test cases for store errors

--- internal/config/configurator_test.go
+++ internal/config/configurator_test.go
@@ -6,18 +6,44 @@ import (
 	"testing"
 
 	"github.com/adavidalbertson/gpair/internal/store"
+	"github.com/pkg/errors"
 )
 
-var mockStore *store.InMemoryStore
-
-func setUp() {
+func mockStore() store.Store {
 	startingConfig := populateConfig()
 
 	configJSON, _ := json.Marshal(startingConfig)
 	configBytes := []byte(configJSON)
 
-	mockStore = &store.InMemoryStore{}
-	mockStore.Write(configBytes)
+	mockStore := &store.InMemoryStore{}
+	err := mockStore.Write(configBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return mockStore
+}
+
+type readErrorStore struct {
+	store.Store
+}
+
+func (res readErrorStore) Read() ([]byte, error) {
+	return nil, errors.New("fake read error")
+}
+func readErrorMockStore() store.Store {
+	return readErrorStore{mockStore()}
+}
+
+type writeErrorStore struct {
+	store.Store
+}
+
+func (wes writeErrorStore) Write([]byte) error {
+	return errors.New("fake write error")
+}
+func writeErrorMockStore() store.Store {
+	return writeErrorStore{mockStore()}
 }
 
 func populateConfig() Config {
@@ -30,24 +56,24 @@ func populateConfig() Config {
 }
 
 func Test_configurator_GetCollaborators(t *testing.T) {
-	setUp()
-
 	tests := []struct {
 		name    string
 		aliases []string
+		store   store.Store
 		want    []Collaborator
 		wantErr bool
 	}{
-		{"one alias", []string{"a2"}, []Collaborator{NewCollaborator("name2", "email2")}, false},
-		{"two alias", []string{"a3", "a1"}, []Collaborator{NewCollaborator("name3", "email3"), NewCollaborator("name1", "email1")}, false},
-		{"nonexistent alias", []string{"a4"}, nil, true},
-		{"nonexistent aliases", []string{"a4", "a5"}, nil, true},
-		{"empty args", []string{}, nil, false},
+		{"one alias", []string{"a2"}, mockStore(), []Collaborator{NewCollaborator("name2", "email2")}, false},
+		{"two alias", []string{"a3", "a1"}, mockStore(), []Collaborator{NewCollaborator("name3", "email3"), NewCollaborator("name1", "email1")}, false},
+		{"nonexistent alias", []string{"a4"}, mockStore(), nil, true},
+		{"nonexistent aliases", []string{"a4", "a5"}, mockStore(), nil, true},
+		{"empty args", []string{}, mockStore(), nil, false},
+		{"read error", []string{"a1"}, readErrorMockStore(), nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := configurator{
-				store: mockStore,
+				store: tt.store,
 			}
 			got, err := c.GetCollaborators(tt.aliases...)
 			if (err != nil) != tt.wantErr {
@@ -70,34 +96,39 @@ func Test_configurator_AddCollaborator(t *testing.T) {
 	type testCase struct {
 		name    string
 		args    args
+		store   store.Store
 		want    Config
 		wantErr bool
 	}
 
-	newTestCase := func(name, alias string, collab Collaborator, wantErr bool) testCase {
+	newTestCase := func(name, alias string, collab Collaborator, store store.Store, wantErr bool) testCase {
 		wantConfig := populateConfig()
 		wantConfig.Collaborators[alias] = collab
 
 		return testCase{
 			name:    name,
 			args:    args{alias, collab},
+			store:   store,
 			want:    wantConfig,
 			wantErr: wantErr,
 		}
 	}
 
 	tests := []testCase{
-		newTestCase("add new", "a4", NewCollaborator("name4", "email4"), false),
-		newTestCase("add existing", "a2", NewCollaborator("name4", "email4"), false),
+		newTestCase("add new", "a4", NewCollaborator("name4", "email4"), mockStore(), false),
+		newTestCase("add existing", "a2", NewCollaborator("name4", "email4"), mockStore(), false),
+
+		// If loading fails, AddCollaborator should return an error, and load should return an empty config
+		{"add read error", args{"a4", NewCollaborator("name4", "email4")}, readErrorMockStore(), NewConfig(), true},
+
+		// If saving fails, AddCollaborator should return an error, and the config should be unaltered
+		{"add write error", args{"a4", NewCollaborator("name4", "email4")}, writeErrorMockStore(), populateConfig(), true},
 	}
 
 	for _, tt := range tests {
-
-		setUp()
-
 		t.Run(tt.name, func(t *testing.T) {
 			c := configurator{
-				store: mockStore,
+				store: tt.store,
 			}
 			if err := c.AddCollaborator(tt.args.alias, tt.args.collaborator); (err != nil) != tt.wantErr {
 				t.Errorf("configurator.AddCollaborator() error = %v, wantErr %v", err, tt.wantErr)
@@ -115,12 +146,13 @@ func Test_configurator_DeleteCollaborators(t *testing.T) {
 	type testCase struct {
 		name        string
 		aliases     []string
+		store       store.Store
 		wantDeleted []string
 		wantConfig  Config
 		wantErr     bool
 	}
 
-	newTestCase := func(name string, aliases, wantDeleted []string, wantErr bool) testCase {
+	newTestCase := func(name string, aliases []string, store store.Store, wantDeleted []string, wantErr bool) testCase {
 		wantConfig := populateConfig()
 
 		for _, alias := range aliases {
@@ -130,6 +162,7 @@ func Test_configurator_DeleteCollaborators(t *testing.T) {
 		return testCase{
 			name:        name,
 			aliases:     aliases,
+			store:       store,
 			wantDeleted: wantDeleted,
 			wantConfig:  wantConfig,
 			wantErr:     wantErr,
@@ -137,20 +170,23 @@ func Test_configurator_DeleteCollaborators(t *testing.T) {
 	}
 
 	tests := []testCase{
-		newTestCase("delete one", []string{"a2"}, []string{"a2"}, false),
-		newTestCase("delete two", []string{"a3", "a1"}, []string{"a3", "a1"}, false),
-		newTestCase("delete nonexistent", []string{"a4"}, nil, true),
-		newTestCase("delete nonexistents", []string{"a4", "a5"}, nil, true),
-		newTestCase("delete mixed", []string{"a2", "a4"}, []string{"a2"}, true),
+		newTestCase("delete one", []string{"a2"}, mockStore(), []string{"a2"}, false),
+		newTestCase("delete two", []string{"a3", "a1"}, mockStore(), []string{"a3", "a1"}, false),
+		newTestCase("delete nonexistent", []string{"a4"}, mockStore(), nil, true),
+		newTestCase("delete nonexistents", []string{"a4", "a5"}, mockStore(), nil, true),
+		newTestCase("delete mixed", []string{"a2", "a4"}, mockStore(), []string{"a2"}, true),
+
+		// if loading fails, DeleteCollaborators should return nil and an error, and load will return an empty config
+		{"delete read error", []string{"a2"}, readErrorMockStore(), nil, NewConfig(), true},
+
+		// if saving fails, DeleteCollaborators should return nil and an error, and the config should be unaltered
+		{"delete write error", []string{"a2"}, writeErrorMockStore(), nil, populateConfig(), true},
 	}
 
 	for _, tt := range tests {
-
-		setUp()
-
 		t.Run(tt.name, func(t *testing.T) {
 			c := configurator{
-				store: mockStore,
+				store: tt.store,
 			}
 			gotDeleted, err := c.DeleteCollaborators(tt.aliases...)
 			if (err != nil) != tt.wantErr {
@@ -159,6 +195,65 @@ func Test_configurator_DeleteCollaborators(t *testing.T) {
 			}
 			if !reflect.DeepEqual(gotDeleted, tt.wantDeleted) {
 				t.Errorf("configurator.DeleteCollaborators() = %v, want %v", gotDeleted, tt.wantDeleted)
+			}
+
+			gotConfig, _ := c.load()
+			if !reflect.DeepEqual(gotConfig, tt.wantConfig) {
+				t.Errorf("configurator.load() = %v, want %v", gotConfig, tt.wantConfig)
+			}
+		})
+	}
+}
+
+func Test_configurator_load(t *testing.T) {
+	tests := []struct {
+		name    string
+		store   store.Store
+		want    Config
+		wantErr bool
+	}{
+		{"happy path", mockStore(), populateConfig(), false},
+		{"read error", readErrorMockStore(), NewConfig(), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := configurator{
+				store: tt.store,
+			}
+			got, err := c.load()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("configurator.load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("configurator.load() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_configurator_save(t *testing.T) {
+	type args struct {
+		config Config
+	}
+	tests := []struct {
+		name       string
+		store      store.Store
+		args       args
+		wantConfig Config
+		wantErr    bool
+	}{
+		{"happy path", mockStore(), args{populateConfig()}, populateConfig(), false},
+		{"empty config", mockStore(), args{NewConfig()}, NewConfig(), false},
+		{"write error", writeErrorMockStore(), args{populateConfig()}, populateConfig(), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := configurator{
+				store: tt.store,
+			}
+			if err := c.save(tt.args.config); (err != nil) != tt.wantErr {
+				t.Errorf("configurator.save() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
 			gotConfig, _ := c.load()
